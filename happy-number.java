a8abfcd120288bf529853ecf6d499e12252034d9
Daeyeol Jo <41155899+daeyeolJo@users.noreply.github.com>
2024-01-21 13:24:00 +0900

    Create happy-number.java

---
+++ happy-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while(n != 1){
+            int sum = 0;
+            while(n != 0){
+                sum += Math.pow(n % 10, 2);
+                n /= 10;
+            }
+            n = sum;
+            if(set.contains(n))
+                return false;
+            set.add(n);
+        }
+        return true;
+    }
+}
